5cc79a4e8a273c0538b6865172dfd498ff61c706
Liz Starin <liz.starin@gmail.com>
2016-02-23 10:51:25 -0500

    re-establishes lost connection

--- fezzik.py
+++ fezzik.py
@@ -1,4 +1,5 @@
 from twitter import *
+import threading
 import os, tweet_parser
 
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
@@ -9,33 +10,60 @@ CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
 twitter = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 twitter_stream = TwitterStream(auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 
-iterator = twitter_stream.statuses.sample()
-
 potential_matches = []
+matches = []
 
 def find_rhyme(tweet):
 	t = tweet_parser.build_tweet_info(tweet)
 
 	for pm in potential_matches:
 		if t['last_syllable'] == pm['last_syllable'] and len(t['meter']) == len(pm['meter']) and t['last_word'] != pm['last_word']:
-			create_rhyme(t, pm)
+			potential_matches.remove(pm)
+			archive_rhyme(t, pm)
+			print 'match found'
 			return
 
 	potential_matches.append(t)
 
-def create_rhyme(t, m):
-	potential_matches.remove(m)
+def archive_rhyme(t, m):
+	matches.append([t, m])
 
-	print t['text']
-	print m['text']
+def tweet_rhyme(r):
+	print r[0]['text']
+	print r[1]['text']
 	print '\n'
 
-	twitter.statuses.retweet(_id=t['_id'])
-	twitter.statuses.retweet(_id=m['_id'])
-
-for tweet in iterator:
 	try:
-		if tweet['lang'] == 'en':
-			find_rhyme(tweet)
-	except:
-		pass
+		twitter.statuses.retweet(_id=r[0]['_id'])
+		twitter.statuses.retweet(_id=r[1]['_id'])
+	except TwitterHTTPError as e:
+		print e
+
+def tweet_rhymes():
+	threading.Timer(600, tweet_rhymes).start()
+  	if len(matches) > 0:
+  		tweet_rhyme(matches[0])
+  		matches.remove(matches[0])
+  	else:
+  		print 'nothing to tweet'
+
+def iterate(iterator):
+	tweet_rhymes()
+
+	for tweet in iterator:
+		if 'hangup' in tweet:
+			print 'hanging up and calling again \n'
+			iterate(twitter_stream.statuses.sample())
+		else:
+			try:
+				if tweet['lang'] == 'en' and '@' not in tweet['text'] and '#' not in tweet['text']:
+					find_rhyme(tweet)
+			except:
+				pass
+
+def destroy_tweets(): # utility function
+	tweets = twitter.statuses.user_timeline(screen_name='anybdywannapnut', count=200)
+	for t in tweets:
+		twitter.statuses.destroy(id=t['id'])
+
+iterate(twitter_stream.statuses.sample())
